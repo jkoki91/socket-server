64e41537aaae76de16c780eccc0fe55446e37f7f
Jeff Worker <>
2023-02-06 18:48:07 +0100

    socket server

---
+++ public/js/socket-client.js
@@ -0,0 +1,50 @@
+
+
+// REferencias del html
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+const textMensaje = document.querySelector('#textMensaje');
+const btnEnviar = document.querySelector('#btnEnviar');
+
+
+const socket = io();
+
+socket.on('connect', () => {
+
+    lblOffline.style.display = 'none'
+    lblOnline.style.display = ''
+});
+
+socket.on('disconnect', () => {
+
+    lblOnline.style.display = 'none'
+    lblOffline.style.display = ''
+
+});
+
+socket.on('enviar-mensaje', (payload) => {
+
+    console.log(payload);
+
+});
+
+btnEnviar.addEventListener('click', () => {
+    const mensaje = textMensaje.value
+    const payload = {
+        mensaje,
+        id: '123abc',
+        fecha: new Date().getTime()
+    }
+
+    socket.emit( 'enviar-mensaje', payload, (id) => {
+
+        console.log('Desde el server', id);
+
+    });
+})
+
+
+
+
+
+
